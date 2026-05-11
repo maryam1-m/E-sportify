c58fca412afed3445d4cacfdd21f33202526bbfc
Fawzia AbuAsbah <s12219765@stu.najah.edu>
2026-05-11 23:14:08 +0300

    integrete part of dashboard

--- src/main/java/org/example/controller/DashboardController.java
+++ src/main/java/org/example/controller/DashboardController.java
@@ -4,34 +4,35 @@ import javafx.event.ActionEvent;
 
 public class DashboardController {
 
+    public void goToDashboard(ActionEvent event) throws Exception {
+        Navigation.goTo(event, "dashboardInterface.fxml");
+    }
+
     public void goToPlayers(ActionEvent event) throws Exception {
-        System.out.println("Players clicked");
-        // Navigation.goTo(event, "players.fxml");
+        Navigation.goTo(event, "Players.fxml");
     }
 
     public void goToTeams(ActionEvent event) throws Exception {
         System.out.println("Teams clicked");
-        // Navigation.goTo(event, "teams.fxml");
+        // Navigation.goTo(event, "Teams.fxml");
     }
 
     public void goToTournaments(ActionEvent event) throws Exception {
         System.out.println("Tournaments clicked");
-        // Navigation.goTo(event, "tournaments.fxml");
+        // Navigation.goTo(event, "Tournaments.fxml");
     }
 
     public void goToMatches(ActionEvent event) throws Exception {
         System.out.println("Matches clicked");
-        // Navigation.goTo(event, "matches.fxml");
+        // Navigation.goTo(event, "Matches.fxml");
     }
 
     public void goToGames(ActionEvent event) throws Exception {
-        System.out.println("Games clicked");
-        // Navigation.goTo(event, "games.fxml");
+        Navigation.goTo(event, "Games.fxml");
     }
 
     public void goToSeasons(ActionEvent event) throws Exception {
-        System.out.println("Seasons clicked");
-        // Navigation.goTo(event, "seasons.fxml");
+        Navigation.goTo(event, "Seasons.fxml");
     }
 
     public void logout(ActionEvent event) throws Exception {
