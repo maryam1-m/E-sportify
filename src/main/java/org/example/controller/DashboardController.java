8cd07665a7d54bce1e4fceb73bcdbe43c6566616
Fawzia AbuAsbah <s12219765@stu.najah.edu>
2026-05-12 00:24:27 +0300

    Integration in user done

--- src/main/java/org/example/controller/DashboardController.java
+++ src/main/java/org/example/controller/DashboardController.java
@@ -13,18 +13,15 @@ public class DashboardController {
     }
 
     public void goToTeams(ActionEvent event) throws Exception {
-        System.out.println("Teams clicked");
-        // Navigation.goTo(event, "Teams.fxml");
+        Navigation.goTo(event, "Teams.fxml");
     }
 
     public void goToTournaments(ActionEvent event) throws Exception {
-        System.out.println("Tournaments clicked");
-        // Navigation.goTo(event, "Tournaments.fxml");
+        Navigation.goTo(event, "Tournaments.fxml");
     }
 
     public void goToMatches(ActionEvent event) throws Exception {
-        System.out.println("Matches clicked");
-        // Navigation.goTo(event, "Matches.fxml");
+        Navigation.goTo(event, "Matches.fxml");
     }
 
     public void goToGames(ActionEvent event) throws Exception {
